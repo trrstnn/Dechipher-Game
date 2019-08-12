8830e7c05c2b7042a1ed99c9a88e203a62be68d5
trrstnn <edeclarin@gmail.com>
2019-08-12 11:48:20 -0700

    adds test code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ let game;
+ const startButton = document.getElementById('btn__reset');
+ startButton.addEventListener('click', game => game = new Game().startGame())
+ 
